83b491bc4bf2f8f46cc40ebb686e76e8f444fb20
aubint <aubint10@gmail.com>
2014-11-07 12:08:17 -0500

    AppsCuadrado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado,perimetro;
+            Console.WriteLine("HALLAR EL PERIMETRO DE UN CUADRADO");
+
+            Console.WriteLine("Ingrese lado del cuadrado: ");
+            lado = int.Parse(Console.ReadLine());
+            perimetro = lado * 4;
+
+            Console.WriteLine("El perimetro del cuadrado es: "+perimetro);
+            Console.ReadKey();
+
+        }
+    }
+}
